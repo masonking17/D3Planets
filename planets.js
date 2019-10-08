00a2b28d821bd1cf6ae9c335989941509585f711
Lucas Williamson <lucas.williamson@centre.edu>
2019-10-08 11:03:05 -0400

    did an 8

--- planets.js
+++ planets.js
@@ -1,40 +1,66 @@
+var Names = ["string1", "string2", "string3"];
 
-
-
-
-
-
-/*
-var makePlanet = function(name,img,distance,radius,density,moons)
-{
-    return {name,img,distance,radius,density,moons};
+var namesAppend = function(){
+   d3.select("#A1").append("p").text(Names); 
+    
 }
 
+namesAppend();
 
-var planets = [
-makePlanet("Mercury","img/mercury.jpg",
-.38,.38,5.43,0),
-makePlanet("Venus","img/venus.jpg",
-.72,.94,5.24,0),
-makePlanet("Earth","img/earth.jpg",
-1,1,5.52,1),
-makePlanet("Mars","img/mars.jpg",
-1.52,.53,3.94,2),
-makePlanet("Jupiter","img/jupiter.jpg",
-5.2,11.21,1.33,79),
-makePlanet("Saturn","img/saturn.jpg",
-9.53,9.45,.7,62),
-makePlanet("Uranus","img/uranus.jpg",
-19.19,4.0,1.3,27),
-makePlanet("Neptune","img/neptune.jpg",
-30,3.88,1.76,14),
-];
+var pAppend = function(d){
 
-console.log(JSON.stringify(planets,null,2))
+    d3.select("#A2")
+        .selectAll("span")
+        .data(d)
+        .enter()
+        .append("span")
+        .text(function(d){
+        return d
+    });
+};
+pAppend(Names);
 
-*/
+var planetPromise=d3.json("planets.json");
 
+planetPromise.then(
+function(d)
+{
+    drawPlanets(d);
+    planetNames(d);
+    console.log("works", d);
+    
+},
+    function(err){
+    console.log("broke",err);
+    }
+);
 
+var drawPlanets = function (d){
+    var pDiv = d3.select("#B3");
+    pDiv.selectAll("img")
+        .data(d)
+        .enter()
+        .append("img")
+        .attr("src",function(d){
+        return d.img
+    });
+};
 
+var makeOl = function(){
+    d3.select("#B4").append("ol").attr("id", "nameslist");
+};
 
+var planetNames = function (d) {
+    makeOl();
+    var pOl = d3.select("#nameslist");
+    pOl
+        .selectAll("li")
+        .data(d)
+        .enter()
+        .append("li")
+        .text(function(d){return d.name});
+    
+
+    
+};
                       
