11b251524777bc59d4a73fa1f32eba59ca2dab48
Lucas Williamson <lucas.williamson@centre.edu>
2019-10-09 22:44:36 -0400

    added tables

--- planets.js
+++ planets.js
@@ -20,7 +20,7 @@ var pAppend = function(d){
 };
 pAppend(Names);
 
-var planetPromise=d3.json("https://masonking17.github.io/D3Planets/planets.json");
+var planetPromise=d3.json("planets.json");
 
 planetPromise.then(
 function(d)
@@ -28,6 +28,8 @@ function(d)
     drawPlanets(d);
     planetNames(d);
     makeTable(d);
+    makeTableC2(d);
+    makeTableC3(d);
     
     console.log("works", d);
     
@@ -75,13 +77,79 @@ var makeTable = function (d){
         .data(d)
         .enter()
         .append("tr")
-        .attr("class", "nameColumn");
-     
-    d3.selectAll(".nameColumn")
-        .selectAll("td")
+};
+
+var makeTableC2 = function(d){
+    d3.select("#C2")
+        .append("table")
+        .attr("id", "nameTable");
+    d3.select("#nameTable")
+        .selectAll("tr")
         .data(d)
         .enter()
+        .append("tr")
+        .attr("class", "nameColumn");
+    d3.selectAll(".nameColumn")
         .append("td")
         .text(function(d){return d.name});
 };
+
+var makeTableC3 = function(d) {
+    d3.select("#C3")
+        .append("table")
+        .attr("id", "totalTable");
+   d3.select("#totalTable")
+        .append("thead")
+        .attr("id", "totalThead");
+    d3.select("#totalTable")
+        .append("tbody")
+        .attr("id", "totalTbody");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Planet Name");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Planet Image");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Distance from Sun (AU)");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Radius Ratio to Earth");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Planet Density (g/cm^3)");
+    d3.select("#totalThead")
+        .append("th")
+        .text("Number of Moons");
+    d3.select("#totalTbody")
+        .selectAll("tr")
+        .data(d)
+        .enter()
+        .append("tr")
+        .attr("class", "rows");
+    d3.selectAll(".rows")
+        .append("td")
+        .text(function(d){return d.name});
+    d3.selectAll(".rows")
+        .append("td")
+        .attr("class", "picColumn");
+    d3.selectAll(".picColumn")
+        .append("img")
+        .attr("src", function(d){return d.img});
+    d3.selectAll(".rows")
+        .append("td")
+        .text(function(d){return d.distance});
+    d3.selectAll(".rows")
+        .append("td")
+        .text(function(d){return d.radius});
+    d3.selectAll(".rows")
+        .append("td")
+        .text(function(d){return d.density});
+    d3.selectAll(".rows")
+        .append("td")
+        .text(function(d){return d.moons});
+    
+    
+}
 
